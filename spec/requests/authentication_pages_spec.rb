c2e045b9d6d8d300dcf9be7ce0cb3362b3ec4a2e
Chris Malin <csmalin@gmail.com>
2012-08-21 18:10:18 +0400

    Add user following, wasnt as bad as chapter 10

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -50,6 +50,18 @@ describe "with valid information" do
 describe "for non-signed-in users" do
   let(:user) { FactoryGirl.create(:user) }
 
+  describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+
   describe "when attempting to visit a protected page" do
     before do
       visit edit_user_path(user)
@@ -59,7 +71,6 @@ describe "for non-signed-in users" do
     end
 
     describe "after signing in" do
-
       it "should render the desired protected page" do
         page.should have_selector('title', text: 'Edit user')
       end
@@ -79,6 +90,16 @@ describe "for non-signed-in users" do
       specify { response.should redirect_to(signin_path) }
     end
 
+    describe "visiting the following page" do
+      before { visit following_user_path(user) }
+      it { should have_selector('title', text: 'Sign in') }
+     end
+
+     describe "visiting the followers page" do
+       before { visit followers_user_path(user) }
+       it { should have_selector('title', text: 'Sign in') }
+     end
+    
   describe "visiting the user index" do
       before { visit users_path }
       it { should have_selector('title', text: 'Sign in') }
