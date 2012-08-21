c17ebe7f7221451d0275cbc1bd90a79074cec178
Chris Malin <csmalin@gmail.com>
2012-08-21 05:15:33 +0400

    Add user microposts, this chapter sucked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
- 
-  def show
-	@user = User.find(params[:id])
-  end
-  
-  def index
+
+def index
     @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -23,10 +24,21 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
-     end
-   end
+    end
+  end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -35,32 +47,16 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  def update
-    if @user.update_attributes(params[:user])
-       flash[:success] = "Profile updated"
-       sign_in @user
-       redirect_to @user
-    else
-      render 'edit'
-    end
-   end
 
-   private
 
-     def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+  private
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
-	redirect_to(root_path) unless current_user.admin?
+      redirect_to(root_url) unless current_user.admin?
     end
-   
 end
